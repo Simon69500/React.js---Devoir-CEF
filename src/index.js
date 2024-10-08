240ef198ea594eff4a3eefc0fad704565c8a3545
Simon69500 <150716564+Simon69500@users.noreply.github.com>
2024-10-08 19:10:19 +0200

    Fix font path and rebuild

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import './fontAwesomeConfig'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './SCSS/Variables.scss';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
